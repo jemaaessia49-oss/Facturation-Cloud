a24a239c2976ff26ab2ae72a0a5bc494ac7ccc30
jemaaessia49-oss <jemaaessia49@gmail.com>
2026-08-08 01:02:52 +0100

    CRUD Sociétés fonctionnel

---
+++ src/Controller/Admin/SocieteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Societe;
+use App\Form\SocieteType;
+use App\Repository\SocieteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/societes')]
+class SocieteController extends AbstractController
+{
+    #[Route('', name: 'app_admin_societe_index', methods: ['GET'])]
+    public function index(SocieteRepository $societeRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        return $this->render('admin/societe/index.html.twig', [
+            'societes' => $societeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/nouveau', name: 'app_admin_societe_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $societe = new Societe();
+        $form = $this->createForm(SocieteType::class, $societe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($societe);
+            $em->flush();
+
+            $this->addFlash('success', 'Société créée avec succès.');
+
+            return $this->redirectToRoute('app_admin_societe_index');
+        }
+
+        return $this->render('admin/societe/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/modifier', name: 'app_admin_societe_edit', methods: ['GET', 'POST'])]
+    public function edit(Societe $societe, Request $request, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $form = $this->createForm(SocieteType::class, $societe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Société modifiée avec succès.');
+
+            return $this->redirectToRoute('app_admin_societe_index');
+        }
+
+        return $this->render('admin/societe/edit.html.twig', [
+            'form' => $form,
+            'societe' => $societe,
+        ]);
+    }
+
+    #[Route('/{id}/supprimer', name: 'app_admin_societe_delete', methods: ['POST'])]
+    public function delete(Societe $societe, Request $request, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        if ($this->isCsrfTokenValid('delete_societe_'.$societe->getId(), $request->getPayload()->getString('_token'))) {
+            $em->remove($societe);
+            $em->flush();
+            $this->addFlash('success', 'Société supprimée.');
+        }
+
+        return $this->redirectToRoute('app_admin_societe_index');
+    }
+}
--- src/Form/SocieteType.php
+++ src/Form/SocieteType.php
@@ -4,16 +4,28 @@ namespace App\Form;
 
 use App\Entity\Societe;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class SocieteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('informationsGenerales')
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la société',
+                'constraints' => [
+                    new NotBlank(['message' => 'Merci de saisir le nom de la société']),
+                ],
+            ])
+            ->add('informationsGenerales', TextareaType::class, [
+                'label' => 'Informations générales',
+                'required' => false,
+                'attr' => ['rows' => 4],
+            ])
         ;
     }
 
